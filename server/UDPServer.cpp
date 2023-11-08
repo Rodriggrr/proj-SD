86c0319d643e8c7079bbf608f4cc45a0101bd7b0
Rodriggrr <rfarinon@alu.ufc.br>
2023-11-08 11:45:29 -0300

    up

--- server/UDPServer.cpp
+++ server/UDPServer.cpp
@@ -1,3 +1,14 @@
-#include "../include/Classes.pb.h"
+#include "src/DatagramSocket.hpp"
+#include "src/Campeonato.hpp"
+#include "src/fn.hpp"
 #include <iostream>
 #include <thread>
+
+using namespace Gerenciador;
+int main(){
+    Campeonato campeonato;
+    Time time;
+    time.set_nome("Flamengo");
+    time.set_pontos(6);
+    time.set_qtdjogos(2);
+}
---
+++ server/src/Campeonato.hpp
@@ -0,0 +1,40 @@
+#include <vector>
+#include "../../include/Classes.pb.h"
+
+class Campeonato {
+    std::vector<Gerenciador::Time> times;
+
+public:
+    Gerenciador::Atleta getAtleta(std::string nome){
+        for (auto time : times){
+            for (auto atleta : time.atletas()){
+                if (atleta.first == nome){
+                    return atleta.second;
+                }
+            }
+        }
+        throw std::runtime_error("Atleta não encontrado");
+    }
+    void addTime(Gerenciador::Time time){
+        times.push_back(time);
+    }
+    void addAtleta(std::string time, Gerenciador::Atleta atleta){
+        for(int i = 0; i < times.size(); i++){
+            if (times[i].nome() == time){
+                times[i].mutable_atletas()->insert({atleta.nome(), atleta});
+                return;
+            }
+        }
+        throw std::runtime_error("Time não encontrado");
+    }
+    std::vector<Gerenciador::Atleta> getAllAtletas() {
+        std::vector<Gerenciador::Atleta> atletas;
+        for (auto time : times){
+            for (auto atleta : time.atletas()){
+                atletas.push_back(atleta.second);
+            }
+        }
+        return atletas;
+    }
+
+};
